ffc1e4d030220e76e180b45fd09ba2ccb576cd04
AceArnold <a.mutara@alustudent.com>
2025-01-27 09:12:14 +0000

    just added the_coder.sh

---
+++ the_coder.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#the following line is dir creation "student_directory"
+
+mkdir student_directory
+#next line
+
+touch student_directory/cohort1.txt
+
+touch student_directory/cohort2.txt
+
+touch student_directory/cohort3.txt
+
