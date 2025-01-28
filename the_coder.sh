bdc00cf48bef1a16529159866921a655eec3a011
Jonathan2055 <j.mugisha3@alustudent.com>
2025-01-28 22:30:55 +0200

    I added 3 names in the cohort 1 file

--- the_coder.sh
+++ the_coder.sh
@@ -8,6 +8,8 @@ touch students_directory/cohort-1.txt
 touch students_directory/cohort-2.txt
 touch students_directory/cohort-3.txt
 
-# Adding my name to the cohort 2 file
-vi "Samuel Chima" students_directory/cohort-2.txt 
+# Adding names to the cohort 1 file
+echo "Samuel Chima" >> students_directory/cohort-1.txt
+echo "Arnould Mutara" >> students_directory/cohort-1.txt
+echo "Ngarambe David" >> students_directory/cohort-1.txt
 
