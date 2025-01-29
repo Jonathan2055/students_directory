e500dd889825bc4aeccd926dd60ca40a42a78dd5
AceArnold <a.mutara@alustudent.com>
2025-01-29 02:45:11 -0800

    created a new file the_coder2.txt to add the names

---
+++ the_coder2.sh
@@ -0,0 +1,13 @@
+echo  "Samuel Chima" >> student_directory/cohort-2.txt 
+echo "John Okafor" >> student_directory/cohort-2.txt
+echo "Jean Anne" >> student_directory/cohort-2.txt
+
+# Adding names to the cohort 1 file
+echo "Samuel Chima" >> student_directory/cohort-1.txt
+echo "Arnould Mutara" >> student_directory/cohort-1.txt
+echo "Ngarambe David" >> student_directory/cohort-1.txt
+
+# Adding the names to the cohort 3 file
+echo "Jonathan Mugisha" >> student_directory/cohort-3.txt
+echo "Peter Micheal" >> student_directory/cohort-3.txt
+echo "Keza Lina" >> student_directory/cohort-3.txt
